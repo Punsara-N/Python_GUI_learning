1afa508b7b6fcef4014faad36e01ce5339618846
Punsara-N <37718963+Punsara-N@users.noreply.github.com>
2018-03-27 13:04:16 +0100

    Updated currentTime module
    Added a new functions to the currentTime module so it can output the timestamp as a string seperated by underscores (so it can be used to easily create file names etc.).

--- currentTime.py
+++ currentTime.py
@@ -2,3 +2,21 @@ from datetime import datetime
 
 def currentTime():
     return str(datetime.now())[:23]
+
+def currentTimeText():
+    a = str(datetime.now())[:23]
+    c = _addUnderscore('-', a)
+    c = _addUnderscore(' ', c)
+    c = _addUnderscore(':', c)
+    c = _addUnderscore('.', c)
+    return c
+
+def _addUnderscore(replaceThis, x):
+    b = x.split(replaceThis)
+    c = ''
+    for i in range(len(b)):
+        c = c + '_' + b[i]
+    return c[1:]
+    
+if __name__ == '__main__':
+    print(currentTimeText())
